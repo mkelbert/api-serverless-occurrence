33482b267f90ceb40d6fd04c177f8e71febc792f
Kelvin Oenning <kelvinstang@hotmail.com>
2017-09-18 20:18:24 -0300

    update project

--- handler/user.js
+++ handler/user.js
@@ -1,13 +1,27 @@
 'use strict';
 
+const config = require('../config.js');
+const AWS = require('aws-sdk')
+const DB = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 module.exports.new = (event, context, callback) => {
-  if(!event.email || !event.password)
-  resp(200, {message: 'teste'}, callback);
+  DB.putItem({
+    TableName: 'DB_USER_TESTE1',
+    Item: {
+      email: 'kelvinstang@hotmail.com',
+      name: 'kelvin oenning'
+    }
+  }, (err, data) => {
+    if(err){
+      return resp(500, err, callback);
+    }
+    return resp(200, {message: 'Adicionado com sucesso'}, callback);
+  })
 };
 
 let resp = (statusCode, obj, callback) => {
   callback(null, {
     statusCode,
-    body: JSON.stringify(obj)
+    body: obj
   })
 }
