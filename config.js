a7eff4a46ae4ca3f0ff582b8698e93a61e689956
Kelvin Oenning <kelvinstang@hotmail.com>
2017-09-18 13:10:01 -0300

    teste update

--- config.js
+++ config.js
@@ -4,19 +4,26 @@
 const DEFAULT_BUCKET_PHOTO = 'BUCKET_PHOTO';
 
 /* TABELA DYNAMODB PARA REGISTRO DE OCORRÊNCIAS*/
-const DEFAULT_DB_OCCURRENCE = 'DB_OCURRENCE';
+const DEFAULT_DB_OCCURRENCE = 'DB_OCURRENCE_TESTE1';
+
+const DEFAULT_DB_USER = 'DB_USER_TESTE1';
 
 module.exports.location = () => {
 	return {
 		bucketPhoto: getBucketPhoto(),
-		tableOccurrence: getTableOccurrence()
+		tableOccurrence: getTableOccurrence(),
+		tableUser: getTableUser()
 	}
 }
 
 function getBucketPhoto() {
-	return process.env[DEFAULT_ENV_BUCKET_PHOTO] || DEFAULT_BUCKET_PHOTO;
+	return 'api-serverless-ocurrence-teste-01';
 }
 
 function getTableOccurrence() {
-	return process.env[DEFAULT_ENV_DB_OCCURRENCE] || DEFAULT_DB_OCCURRENCE;
+	return DEFAULT_DB_OCCURRENCE;
+}
+
+function getTableUser() {
+	return DEFAULT_DB_USER;
 }
