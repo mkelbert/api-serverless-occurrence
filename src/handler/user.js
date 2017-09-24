4c55030f9c19e711a658abcccba14fc77196bce7
Kelvin Oenning <kelvinstang@hotmail.com>
2017-09-24 17:38:24 -0300

    update project

--- src/handler/user.js
+++ src/handler/user.js
@@ -1,12 +1,17 @@
 'use strict';
 
 module.exports.new = (event, context, callback) => {
+
+  let arr = [1,2,3,4,5,6,7];
+  let newarr = [];
+  arr.map(i => newarr.push(i))
+
   const response = {
     statusCode: 200,
     headers: {
       "x-custom-header": "My Header Value"
     },
-    body: JSON.stringify({ "message": "Hello World!" })
+    body: newarr
   };
 
   callback(null, response);
