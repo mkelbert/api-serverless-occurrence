578e89d0008eb2c57b637307a1cc3e62623b17b8
Kelvin Oenning <kelvinstang@hotmail.com>
2017-09-25 19:30:25 -0300

    update project

--- tests/Class/user.spec.js
+++ tests/Class/user.spec.js
@@ -1,10 +1,21 @@
-// import { expect } from 'chai';
+import { expect } from 'chai';
 
-// import user from '../../../src/handler/v1/user';
+import User from '../../src/class/User';
 
 describe('User', () => {
   describe('Smoke test', () => {
+    it('should exists User class', () => {
+      expect(User).to.be.a('function');
+    });
 
+    it('should exists property name', () => {
+      const user = new User('kelvin', 'kelvinstang@hotmail.com');
+      expect(user.name).to.exist;
+    });
+
+    it('should exists property email', () => {
+
+    });
   });
 });
 
