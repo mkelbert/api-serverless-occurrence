ec918a0e5f7b8c7c685b1c5acb2bbdd134937e61
Kelvin Oenning <kelvinstang@hotmail.com>
2017-09-25 18:47:49 -0300

    fix user.js

--- src/handler/v1/user.js
+++ src/handler/v1/user.js
@@ -1,18 +1,11 @@
-'use strict';
-
 module.exports.new = (event, context, callback) => {
-
-  let arr = [1,2,3,4,5,6,7];
-  let newarr = [];
-  arr.map(i => newarr.push(i))
-
   const response = {
     statusCode: 200,
     headers: {
-      "x-custom-header": "My Header Value"
+      'x-custom-header': 'My Header Value',
     },
-    body: newarr
+    body: { test: 'test' },
   };
 
   callback(null, response);
-}
+};
