829e1356d38c8827a0ff38a65fab279759e08d27
Kelvin Oenning <kelvinstang@hotmail.com>
2017-09-25 22:14:07 -0300

    upgrade

--- src/class/User.js
+++ src/class/User.js
@@ -1,6 +1,38 @@
+import crypto from 'crypto';
+
 export default class User {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
+  set email(email) {
+    this._email = email;
+  }
+  get email() {
+    return this._email;
+  }
+
+  set token(token) {
+    this._token = token;
+  }
+  get token() {
+    return this._token;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set created(created) {
+    this._created = created;
+  }
+  get created() {
+    return this._created;
+  }
+
+  set password(password) {
+    this._password = crypto.createHash('md5').update(password).digest('hex');
+  }
+  get password() {
+    return this._password;
   }
 }
--- tests/Class/user.spec.js
+++ tests/Class/user.spec.js
@@ -4,18 +4,24 @@ import User from '../../src/class/User';
 
 describe('User', () => {
   describe('Smoke test', () => {
-    it('should exists User class', () => {
-      expect(User).to.be.a('function');
-    });
+    it('should exists User class', () => expect(User).to.be.a('function'));
 
-    it('should exists property name', () => {
-      const user = new User('kelvin', 'kelvinstang@hotmail.com');
-      expect(user.name).to.exist;
-    });
+    it('should exists property name', () => expect(new User()).to.have.property('name'));
 
-    it('should exists property email', () => {
+    it('should exists property email', () => expect(new User).to.have.property('email'));
 
-    });
+    it('should exists property token', () => expect(new User()).to.have.property('token'));
+
+    it('should exists property created', () => expect(new User()).to.have.property('created'));
+
+    it('should exists property password', () => expect(new User()).to.have.property('password'));
   });
-});
 
+  describe('Set password', () => {
+    it('should return the correct data from set value (MD5)', () => {
+      const user = new User();
+      user.password = '12345';
+      expect(user.password).to.be.eq('827ccb0eea8a706c4c34a16891f84e7b')
+    })
+  })
+});
