ca1d1806bc56737e476cc626a101f4270565a011
Kelvin Oenning <kelvinstang@hotmail.com>
2017-10-01 16:56:27 -0300

    update class to remove methods

--- src/models/DB.js
+++ src/models/DB.js
@@ -1,31 +1,6 @@
 import AWS from 'aws-sdk';
 
 export default class DB {
-  static get(arg, db = this.getDB()) {
-    if(!arg) return new Error('The argument object was not informed!');
-    return db.get(arg).promise();
-  }
-
-  static put(arg, db = this.getDB()) {
-    if(!arg) return new Error('The argument object was not informed!');
-    return db.put(arg).promise();
-  }
-
-  static query(arg, db = this.getDB()) {
-    if(!arg) return new Error('The argument object was not informed!');
-    return db.query(arg).promise();
-  }
-
-  static scan(arg, db = this.getDB()) {
-    if(!arg) return new Error('The argument object was not informed!');
-    return db.scan(arg).promise();
-  }
-
-  static delete(arg, db = this.getDB()) {
-    if(!arg) return new Error('The argument object was not informed!');
-    return db.delete(arg).promise();
-  }
-
   static getDB(config = this.getConfigDB()) {
     return new AWS.DynamoDB.DocumentClient(config);
   }
--- tests/models/DB.spec.js
+++ tests/models/DB.spec.js
@@ -9,40 +9,11 @@ chai.use(sinonChai);
 import DB from '../../src/models/DB';
 
 describe('DB', () => {
-
-  let stubedGet, stubedPut, stubedScan, stubedQuery, stubedDelete;
-  let promiseGet, promisePut, promiseScan, promiseQuery, promiseDelete;
-
-  beforeEach(() => {
-    stubedGet = sinon.stub(DB, 'get');
-    stubedPut = sinon.stub(DB, 'put');
-    stubedScan = sinon.stub(DB, 'scan');
-    stubedQuery = sinon.stub(DB, 'query');
-    stubedDelete = sinon.stub(DB, 'delete');
-    promiseGet = stubedGet.returnsPromise();
-    promisePut = stubedPut.returnsPromise();
-    promiseScan = stubedScan.returnsPromise();
-    promiseQuery = stubedQuery.returnsPromise();
-    promiseDelete = stubedDelete.returnsPromise();
-  });
   afterEach(() => {
     delete global.process.env.IS_OFFLINE;
-    stubedGet.restore();
-    stubedPut.restore();
-    stubedScan.restore();
-    stubedQuery.restore();
-    stubedDelete.restore();
   })
 
   describe('Smoke test', () => {
-    it('should exists static (get, put, query, scan, delete) functions', () => {
-      expect(DB.get).to.be.a('function');
-      expect(DB.put).to.be.a('function');
-      expect(DB.query).to.be.a('function');
-      expect(DB.scan).to.be.a('function');
-      expect(DB.delete).to.be.a('function');
-    });
-
     it('should exists static (getDB, getConfigDB) functions', () => {
       expect(DB.getDB).to.be.a('function');
       expect(DB.getConfigDB).to.be.a('function');
@@ -52,7 +23,7 @@ describe('DB', () => {
   describe('Functions', () => {
     describe('getDB', () => {
 
-      it('should returned AWS object must contain (get, put, query, scan, delete) functions', () => {
+      it('should return object must contain (get,put,query,scan,delete) func.', () => {
         expect(DB.getDB().get).to.be.a('function');
         expect(DB.getDB().put).to.be.a('function');
         expect(DB.getDB().query).to.be.a('function');
@@ -76,98 +47,23 @@ describe('DB', () => {
     });
 
     describe('getConfigDB', () => {
-      it('should returned data value must contain region property on DEVELOPER config', () => {
+      it('should returned object must contain region property on (DEV)', () => {
         expect(DB.getConfigDB()).to.have.property('region')
       })
 
-      it('should returned data value must contain region property on PRODUCTION config', () => {
+      it('should returned object must contain region property on (PROD)', () => {
         global.process.env.IS_OFFLINE = true;
         expect(DB.getConfigDB()).to.have.property('region')
       })
 
-      it('should returned data value must contain endpoint property on DEVELOPER config', () => {
+      it('should returned object must contain endpoint property on (DEV)', () => {
         global.process.env.IS_OFFLINE = true;
         expect(DB.getConfigDB()).to.have.property('endpoint')
       })
 
-      it('should returned data value must not contain endpoint property on PRODUCTION config', () => {
+      it('should returned object must not contain endpoint property on (PROD)', () => {
         expect(DB.getConfigDB()).to.not.have.property('endpoint')
       })
-
-    });
-
-    describe('get', () => {
-      it('should return Error if argument is not informed', () => {
-        stubedGet.restore();
-        expect(DB.get()).to.be.a('error');
-        expect(DB.get().message).to.be.equal('The argument object was not informed!');
-      });
-
-      it('should return promise', () => {
-        let promiseResult = { Item: {} }
-        promiseGet.resolves(promiseResult);
-        expect(DB.get({}).resolveValue).to.be.eql(promiseResult);
-      });
-
-    });
-
-    describe('put', () => {
-      it('should return Error if argument is not informed', () => {
-        stubedPut.restore();
-        expect(DB.put()).to.be.a('error');
-        expect(DB.put().message).to.be.equal('The argument object was not informed!');
-      });
-
-      it('should return promise', () => {
-        let promiseResult = { }
-        promisePut.resolves(promiseResult);
-        expect(DB.put({}).resolveValue).to.be.eql(promiseResult);
-      });
-
-    });
-
-    describe('query', () => {
-      it('should return Error if argument is not informed', () => {
-        stubedQuery.restore();
-        expect(DB.query()).to.be.a('error');
-        expect(DB.query().message).to.be.equal('The argument object was not informed!');
-      });
-
-      it('should return promise', () => {
-        let promiseResult = { Items: {} }
-        promiseQuery.resolves(promiseResult);
-        expect(DB.query({}).resolveValue).to.be.eql(promiseResult);
-      });
-
-    });
-
-    describe('scan', () => {
-      it('should return Error if argument is not informed', () => {
-        stubedScan.restore();
-        expect(DB.scan()).to.be.a('error');
-        expect(DB.scan().message).to.be.equal('The argument object was not informed!');
-      });
-
-      it('should return promise', () => {
-        let promiseResult = { Items: {} }
-        promiseScan.resolves(promiseResult);
-        expect(DB.scan({}).resolveValue).to.be.eql(promiseResult);
-      });
-
-    });
-
-    describe('delete', () => {
-      it('should return Error if argument is not informed', () => {
-        stubedDelete.restore();
-        expect(DB.delete()).to.be.a('error');
-        expect(DB.delete().message).to.be.equal('The argument object was not informed!');
-      });
-
-      it('should return promise', () => {
-        let promiseResult = {}
-        promiseDelete.resolves(promiseResult);
-        expect(DB.delete({}).resolveValue).to.be.eql(promiseResult);
-      });
 
     });
   });
