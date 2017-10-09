918b8b62299d8a79658675fe561d5830ba2c699b
Kelvin Oenning <kelvinstang@hotmail.com>
2017-10-09 12:56:00 -0300

    update user handler

---
+++ src/handler/v1/user.js
@@ -0,0 +1,12 @@
+
+module.exports.new = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "x-custom-header": "My Header Value"
+    },
+    body: JSON.stringify({ "message": "Hello World!" })
+  };
+
+  callback(null, response);
+}
