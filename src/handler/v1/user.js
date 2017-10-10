be9b327469273d2f4c165f76d0e2b990a003f770
Kelvin Oenning <kelvinstang@hotmail.com>
2017-10-10 12:56:49 -0300

    upgrade user handler

--- src/handler/v1/user.js
+++ src/handler/v1/user.js
@@ -2,7 +2,6 @@ import DB from '../../models/DB';
 import project from '../../project';
 
 module.exports.new = (event, context, callback) => {
-
   let parmQuery = {
     TableName: project.config().TABLE_USER,
     KeyConditionExpression: 'email = :email',
@@ -11,14 +10,25 @@ module.exports.new = (event, context, callback) => {
     }
   }
 
-  console.log(event.body.nick)
-
-  DB.get().query(parmQuery).promise()
+  toJson(event.body)
+  .then(body => {
+    event.body = body;
+    Promise.resolve();
+  })
+  .then(() => DB.get().query(parmQuery).promise())
   .then(res => checkUserExist(res))
   .then(() => end(200, event, callback))
   .catch(err => {
     end(500, err, callback)
   });
+}
+
+function toJson(arg){
+  try{
+    return Promise.resolve(JSON.parse(arg));
+  }catch(err){
+    return Promise.resolve(arg);
+  }
 }
 
 function checkUserExist(res){
